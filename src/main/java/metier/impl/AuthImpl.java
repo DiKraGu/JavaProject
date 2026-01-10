7a7ba4ee9aaa20e2eac6d61162a8f1556603d2e6
DikraGuiri <dikra.guiri@gmail.com>
2026-01-10 15:18:24 +0100

    contenus impl metier

--- src/main/java/metier/impl/AuthImpl.java
+++ src/main/java/metier/impl/AuthImpl.java
@@ -1,5 +1,37 @@
 package metier.impl;
 
-public class AuthImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
+import dao.User;
+import exception.AuthException;
+import metier.impl.persistence.JpaUtil;
+import metier.interfaces.IAuth;
+
+public class AuthImpl implements IAuth {
+
+    private EntityManager em;
+
+    public AuthImpl() {
+        this.em = JpaUtil.getEntityManager();
+    }
+
+    @Override
+    public User seConnecter(String email, String motDePasse) throws AuthException {
+        try {
+            TypedQuery<User> q = em.createQuery(
+                    "SELECT u FROM User u WHERE u.email = :email", User.class);
+            q.setParameter("email", email);
+            User u = q.getSingleResult();
+
+            if (!u.getMotDePasse().equals(motDePasse)) {
+                throw new AuthException("Mot de passe incorrect");
+            }
+            return u;
+
+        } catch (NoResultException e) {
+            throw new AuthException("Utilisateur introuvable");
+        }
+    }
 }
--- src/main/java/metier/impl/ClientImpl.java
+++ src/main/java/metier/impl/ClientImpl.java
@@ -1,5 +1,31 @@
 package metier.impl;
 
-public class ClientImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
+import dao.Reparation;
+import exception.NotFoundException;
+import metier.impl.persistence.JpaUtil;
+import metier.interfaces.IClient;
+
+public class ClientImpl implements IClient {
+
+    private EntityManager em;
+
+    public ClientImpl() {
+        this.em = JpaUtil.getEntityManager();
+    }
+
+    @Override
+    public Reparation suivreReparationParCode(String codeSuivi) throws NotFoundException {
+        try {
+            TypedQuery<Reparation> q = em.createQuery(
+                    "SELECT r FROM Reparation r WHERE r.codeSuivi = :code", Reparation.class);
+            q.setParameter("code", codeSuivi);
+            return q.getSingleResult();
+        } catch (NoResultException e) {
+            throw new NotFoundException("Aucune réparation trouvée pour ce code");
+        }
+    }
 }
